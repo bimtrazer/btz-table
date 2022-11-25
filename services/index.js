9eb9f3a7e05759a705f6c11ccb095b544cc3b049
Agustin B. Lozano <a.lozano@bimtrazer.com>
2022-11-25 09:32:51 -0300

    Update services

--- services/index.js
+++ services/index.js
@@ -1,12 +1,24 @@
-async function getTableData() {
-  const URL = 'https://api.example.com/v1/tables';
-  const options = {
-    method: 'GET',
-    DATA: {},
+// Using async/await
+async function getDataFromApi () {
+  const URL = 'http://36.bimtrazer.com/'
+
+  try {
+    const response = await fetch(URL)
+    return await response.json()
+  } catch (error) {
+    console.error(error)
+    return {}
   }
+}
 
-  const response = await fetch(URL, options);
-  const data = await response.json();
+// Using promises
+function getDataFromApi () {
+  const URL = 'http://36.bimtrazer.com/'
 
-  return data;
+  return fetch(URL)
+    .then(response => response.json())
+    .catch(error => {
+      console.error(error)
+      return RESPONSE_EXAMPLE
+    })
 }
